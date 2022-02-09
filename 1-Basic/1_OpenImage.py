b2c2bf643543bbd353f1bdb91b212efd32af15e4
zer0-911 <mohiqbalf5@gmail.com>
2022-02-09 18:37:46 +0700

    First

---
+++ 1-Basic/1_OpenImage.py
@@ -0,0 +1,24 @@
+# /*
+# * @ author Moh. Iqbal Fatchurozi
+# * @ email mohiqbalf5@gmail.com
+# * @ create date 2022-02-09 16: 28: 48
+# * @ modify date 2022-02-09 16: 28: 48
+# * @ desc[file ini adalah file yang berisi fungsi-fungsi yang digunakan untuk mengambil gambar dari kamera, hilangkan komen untuk menjalankan program]
+# */
+
+import cv2 as cv
+
+# # Membaca/Menampilkan gambar
+# img = cv.imread('../img/gojo.jpg')
+# cv.imshow("Dis", img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+# # Mengubah gambar menjadi abu abu (Grayscale)
+# img = cv.imread('../img/gojo.jpg')
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+# Mengubah gambar menjadi blur
